0b390f3a97618a52fc4c41c6f5e3d2db9ab2d9fc
venkatesh.b <venkatesh.b@changepond.com>
2024-03-07 14:49:00 +0530

    fff

---
+++ demodbbuild/dbo/Tables/Orders.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderID]     INT             NOT NULL,
+    [UserID]      INT             NULL,
+    [OrderDate]   DATETIME        NULL,
+    [TotalAmount] DECIMAL (10, 2) NULL,
+    PRIMARY KEY CLUSTERED ([OrderID] ASC),
+    FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([UserID])
+);
+
---
+++ demodbbuild/dbo/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserID]   INT            NOT NULL,
+    [UserName] NVARCHAR (50)  NULL,
+    [Email]    NVARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
